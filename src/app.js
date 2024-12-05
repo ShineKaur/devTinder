7b8b29b12e4fd616844a3c44c0e46a3582b87c79
ShineKaur <shine.kaur28@gmail.com>
2024-12-04 19:06:26 -0500

    jwt and cookie settings

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 const connectDB = require('./config/database');
 const User = require('./models/user');
-const {validateSignUpData} = require('./utils/validation.js');
+const { validateSignUpData } = require('./utils/validation.js');
+const { userAuth } = require('./middlewares/auth.js');
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.post('/signup', async (req, res) => {
     try {
-    //1st STEP: Validation of data
-    validateSignUpData(req);
+        //1st STEP: Validation of data
+        validateSignUpData(req);
 
-    const { firstName, lastName, emailId, password } = req.body;
+        const { firstName, lastName, emailId, password } = req.body;
 
-    //2nd STEP: Encrypt the password
-    const passwordHash = await bcrypt.hash(password, 10);
+        //2nd STEP: Encrypt the password
+        const passwordHash = await bcrypt.hash(password, 10);
 
-    //Create a new instance of the User model.
-    const userObj = new User({
-        firstName, 
-        lastName, 
-        emailId, 
-        password : passwordHash
-    });
+        //Create a new instance of the User model.
+        const userObj = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
 
         await userObj.save();
         //await User.insertMany(req.body); -- if need to add multiple docs. no need to create diff model as in 12 line.
@@ -33,6 +37,57 @@ app.post('/signup', async (req, res) => {
     } catch (error) {
         res.status(400).send('Error saving User: ' + error.message);
     }
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+
+        if (!user) {
+            throw new Error("Invalid credentials!");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (isPasswordValid) {
+
+            //Creat JWT Token
+            const token = jwt.sign({ _id: user._id }, "Dev@Tinder28", {
+                expiresIn : '1d'
+            });
+
+            //send token in cookie
+            res.cookie('token', token, {
+                expires: new Date(Date.now() + 8 * 3600000),
+            });
+
+            res.status(200).send("Login Successful!!!");
+        } else {
+            throw new Error("Invalid credentials!");
+        }
+
+    } catch (err) {
+        res.status(400).send('ERROR: ' + err);
+    }
+});
+
+app.get('/profile', userAuth, async (req, res) => {
+    try {
+        const user = await req.user;
+
+        res.send(user);
+    } catch(error) {
+        console.log('ee')
+        res.status(400).send('ERROR: ' + error.message);
+    }
+    
+});
+
+app.post('/sendConnectionRequest', userAuth, async(req, res) => {
+    const user = await req.user;
+    res.send(user.firstName + ' sent the connection request'); 
 });
 
 app.get('/user', async (req, res) => {
@@ -78,46 +133,23 @@ app.patch('/user/:userId', async (req, res) => {
     try {
         const ALLOWED_UPDATES = ['photoUrl', 'about', 'gender', 'age', 'skills'];
 
-        const isUpdateAllowed = Object.keys(data).every((k) => 
+        const isUpdateAllowed = Object.keys(data).every((k) =>
             ALLOWED_UPDATES.includes(k)
         );
         if (!isUpdateAllowed) {
             throw new Error('Update not allowed');
         }
 
-        if(data?.skills.length > 10) {
+        if (data?.skills.length > 10) {
             throw new Error('Skills cannot be more than 10.');
         }
 
-        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument : "after", runValidators: true });
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after", runValidators: true });
         // or const user = await User.findByIdAndUpdate(userId, data);
         //console.log(user);
         res.send('User updated successfully');
     } catch (error) {
         res.status(400).send('UPDATE FAILED: ' + error.message);
-    }
-});
-
-app.post('/login', async(req, res) => {
-    try { 
-        const { emailId, password } = req.body;
-
-        const user = await User.findOne({ emailId: emailId });
-
-        if(!user) {
-            throw new Error("Invalid credentials!");
-        }
-
-        const validatePassword = await bcrypt.compare(password, user.password);
-
-        if(validatePassword) {
-            res.status(200).send("Login Successful!!!")
-        } else {
-            throw new Error("Invalid credentials!");
-        }
-
-    } catch(err) {
-        res.status(400).send('ERROR: ' + err);
     }
 });
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,14 +1,32 @@
-const adminAuthData = (req, res, next) => {
-    console.log('Admin Auth is getting checked.');
-    const token = "xyz";
-    const isAdminAuthorised = token === "xyz";
-    if(!isAdminAuthorised) {
-        res.status(401).send('Admin not authorised')
-    } else {
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.js');
+
+const userAuth = async (req, res, next) => {
+    try{
+        const { token } = req.cookies;
+        if(!token) {
+            throw new Error('Token is not valid!')
+        }
+
+        const decodedObj = await jwt.verify(token, "Dev@Tinder28");
+        
+        const { _id } = decodedObj;
+
+        const user = User.findById(_id);
+
+        if(!user) {
+            throw new Error('User not found!');
+        }
+
+        req.user = user;
         next();
     }
+    catch(err) {
+        res.status(400).send('ERROR: ' + err.message);
+    }
+    
 }
 
 module.exports = {
-    adminAuthData,
+    userAuth,
 }
